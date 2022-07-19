40d696025274c141654211f1ecb97953a89f150f
bckstrb <rbccstrb@gmail.com>
2022-07-18 18:52:42 -0600

    retreiving data from local storage and final

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,38 +5,35 @@ var timeStamp = $(".time-block");
 var currentTime = moment().hour();
 //console.log(currentTime)
 
-timeStamp.each(function(){
-    var timeblockHour = $(this).attr("id");
-    //console.log(timeblockHour)
-    if (currentTime > timeblockHour) {
-        //console.log("past")
-        $(this).children("textarea").addClass("past");
-    }else if (currentTime < timeblockHour){
-        //console.log("future")
-        $(this).children("textarea").addClass("future");
-    }else {
-        //console.log("current")
-        $(this).children("textarea").addClass("present");
-    }
+//this will determine past, present, future
+timeStamp.each(function () {
+  var timeblockHour = $(this).attr("id");
+  //console.log(timeblockHour)
+  if (currentTime > timeblockHour) {
+    //console.log("past")
+    $(this).children("textarea").addClass("past");
+  } else if (currentTime < timeblockHour) {
+    //console.log("future")
+    $(this).children("textarea").addClass("future");
+  } else {
+    //console.log("current")
+    $(this).children("textarea").addClass("present");
+  }
+
+  //this will retreive from local storage
+  var hourlyContent = localStorage.getItem(timeblockHour);
+  //console.log(hourlyContent);
+  $(this).children("textarea").val(hourlyContent);
 });
 
 var saveBtn = $(".saveBtn");
 
-saveBtn.on("click", function(){
-    //console.log("click")
-    var textAreaContent = $(this).siblings("textarea").val();
-    //console.log(textAreaContent)
-    var timeblockHour = $(this).parent().attr("id");
-    console.log(timeblockHour)
-    localStorage.setItem(timeblockHour, textAreaContent);
-})
-
-
-
-//set color code for past, present, future
-//--function with if statements to indicate time
-//-- is the moment.js needed to base the time within the function 
-
-
-//log it to local storage so that it will remain after refreshed
-//--need an event listener for each row of a column-for loop?
+//this will save to local storage
+saveBtn.on("click", function () {
+  //console.log("click")
+  var textAreaContent = $(this).siblings("textarea").val();
+  //console.log(textAreaContent)
+  var timeblockHour = $(this).parent().attr("id");
+  //console.log(timeblockHour)
+  localStorage.setItem(timeblockHour, textAreaContent);
+});
